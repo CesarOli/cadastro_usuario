f0a0a2cac95162cd80db7b48efcdc2273d87cf7a
CesarOli <cesaraleoliveira@gmail.com>
2023-09-25 23:50:32 -0300

    exclusao arquivo app.py antigo.

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from dotenv import load_dotenv, os
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("chave_secreta")
-DATABASE_URL = os.getenv("postgresql://cesar:123456@localhost/cadastro_de_usuario")
-DEBUG = os.getenv("DEBUG")
