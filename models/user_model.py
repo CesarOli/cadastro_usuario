222a10ec8a5a8b9400453dce72e7060f7d89317c
CesarOli <cesaraleoliveira@gmail.com>
2023-09-26 23:40:30 -0300

    modelo do banco de dados com colunas nome, email, cpf, pis e senha

--- models/user_model.py
+++ models/user_model.py
@@ -2,3 +2,10 @@ from app import db
 
 class Usuário(db.Model):
     __tablename__ = 'usuários'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    cpf = db.Column(db.String(14), unique= True)
+    pis = db.Column(db.String(11), uniqu=True)
+    senha = db.Column(db.String(128))
+
