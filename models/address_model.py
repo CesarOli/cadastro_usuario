b0d7ab06264ec23eb32be9e2add1a160b1c8e65e
CesarOli <cesaraleoliveira@gmail.com>
2023-09-27 14:30:18 -0300

    Implementação da classe 'Endereco' ao modelo do banco de dados com colunas pais, estado, municipio, cep, rua, numero e complemento.

--- models/address_model.py
+++ models/address_model.py
@@ -1,2 +1,12 @@
 from app import db
 
+class Endereco(db.Model):
+    __tablename__ = 'enderecos'
+    id = db.Column(db.Integer, primary_key=True)
+    pais = db.Column(db.String(50))
+    estado = db.Column(db.String(50))
+    mmunicipio = db.Column(db.String(50))
+    cep = db.Column(db.String(10))
+    rua = db.Column(db.String(150))
+    numero = db.Column(db.String(10))
+    complemento = db.Column(db.String(100))
