86f28e6e462bc7274211eb4958210f0f743491a4
CesarOli <cesaraleoliveira@gmail.com>
2023-10-10 23:56:00 -0300

    Inicialização da estrutura models.

---
+++ app/models/user_model.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Usuario(db.Model):
+    __tablename__ = 'usuários'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    cpf = db.Column(db.String(14), unique= True)
+    pis = db.Column(db.String(11), unique=True)
+    senha = db.Column(db.String(128))
+
+    endereco_id = db.Column(db.Integer, db.ForeignKey('endereço_id'))
+    endereco = db.relationship('Endereco', back_populates='usuarios')
