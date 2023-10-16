2e9e5e8ebe8d4281a9128461ba18ea769b6f7091
CesarOli <cesaraleoliveira@gmail.com>
2023-10-15 23:32:52 -0300

    Import de libs e criação da lógica para cadastro de usuário.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,12 @@
-from app import app
+from models import user_model
+from app import app, db
 from flask import request, jsonify
 
 @app.route('/cadastro', methods=['POST'])
+def cadastrar_usuario():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        cpf = request.form['cpf']
+        pis = request.form['pis']
+        senha = request.form['senha']
