2b53160421c14bec118927d9eea1dc7122c8b23d
CesarOli <cesaraleoliveira@gmail.com>
2023-10-15 23:44:24 -0300

    Criação de uma nova instancia do modelo de usuário.

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,21 @@ from models import user_model
 from app import app, db
 from flask import request, jsonify
 
+#criação da rota/url 'cadastro'
 @app.route('/cadastro', methods=['POST'])
+
+#função que cadastra usuários
 def cadastrar_usuario():
+
+    #lógica para adquirir dados do usuário através do formulário
     if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
         cpf = request.form['cpf']
         pis = request.form['pis']
         senha = request.form['senha']
+
+        #criação de uma nova instância do modelo de usuário cadastrado.
+        novo_usuario = user_model(nome=nome, email=email, cpf=cpf, pis=pis, senha=senha)
+
+        
