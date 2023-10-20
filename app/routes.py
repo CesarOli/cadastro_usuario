b8273cfe0bf4e492ab654e8e02a649df03c318f1
CesarOli <cesaraleoliveira@gmail.com>
2023-10-20 16:31:36 -0300

    Campo de validação de PIS no formulário.

--- app/routes.py
+++ app/routes.py
@@ -28,6 +28,7 @@ class CadastroUsuario(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired(), Length(min=2, max=90)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     cpf = StringField('CPF', validators=[DataRequired(), Length(min=11,max=11), Regexp(r'^\d{11}$', message='CPF inválido'), CpfUnico()])
+    pis = StringField('PIS',validators=[DataRequired(), Length(min=11, max=11), Regexp(r'^\d{11}$', message='PIS inválido'), PisUnico()])
     senha =PasswordField('Senha', validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
 
