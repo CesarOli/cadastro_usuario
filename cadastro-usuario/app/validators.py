fd1ebb8c7be1129b7b0724e749ea75dbd116c198
CesarOli <cesaraleoliveira@gmail.com>
2023-11-04 00:04:01 -0300

    criação da classe que verifica a existencia ou não do cpf digitado pelo usuario.

--- cadastro-usuario/app/validators.py
+++ cadastro-usuario/app/validators.py
@@ -1,4 +1,10 @@
 from wtforms.validators import DataRequired, Email, Length, Regexp, ValidationError
 from app import db
 
-
+class CpfUnico(object):
+    def __call__(self, formulario, field):
+        from models.user_model import Usuario
+        
+        usuario = db.session.query(Usuario).filter_by(cpf=field.data).first()
+        if usuario is not None:
+            raise ValidationError('CPF já cadastrado.')
