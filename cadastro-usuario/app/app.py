b9b1c515b18bc2fc9a077f5cffe6ee16e09a169a
CesarOli <cesaraleoliveira@gmail.com>
2023-11-08 23:49:05 -0300

    Retirada da classe responsável pelo cadastro do usuario, e suas retirada das libs importadas.

--- cadastro-usuario/app/app.py
+++ cadastro-usuario/app/app.py
@@ -1,12 +1,10 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, Regexp, ValidationError
 from dotenv import load_dotenv
-import os
 from flask_wtf.csrf import CSRFProtect
+from forms import CadastroUsuario
+from models.user_model import Usuario
 
 load_dotenv()
 
@@ -18,14 +16,6 @@ app.secret_key = "chave_secreta"
 
 db = SQLAlchemy(app)
 
-'''class CadastroUsuario(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired(), Length(min=2, max=90)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    cpf = StringField('CPF', validators=[DataRequired(), Length(min=11,max=11), Regexp(r'^\d{11}$', message='CPF inválido'), CpfUnico()])
-    pis = StringField('PIS',validators=[DataRequired(), Length(min=11, max=11), Regexp(r'^\d{11}$', message='PIS inválido'), PisUnico()])
-    senha =PasswordField('Senha', validators=[DataRequired()])
-    submit = SubmitField('Cadastrar')
-'''
 @app.route('/', methods=['GET'])
 def hello():
     return 'Seja Bem Vindo!!!'
